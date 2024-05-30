6f6a6fca8786eeac8fe8d4c94215cb6300c47931
Duarte Hortinha <duarte.hortinha@protonmail.com>
2024-05-30 13:07:51 +0100

    validations for memes creation added

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,4 +1,7 @@
 class Meme < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo # adicionado devido ao cloudinary 
+  has_one_attached :photo # adicionado devido ao cloudinary
+  validates :title, :photo, :price, presence: true
+  validates :title, length: { minimum: 4, maximum: 50 }
+  validates :description, length: { minimum: 10, maximum: 300 }
 end
